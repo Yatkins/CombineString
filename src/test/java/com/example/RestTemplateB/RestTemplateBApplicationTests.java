3d10f0de408ae0b7b43bcbe20bde617de62db8fc
Yatkins <tzippi8@gmail.com>
2020-12-20 17:21:40 -0600

    Initial commit

---
+++ src/test/java/com/example/RestTemplateB/RestTemplateBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestTemplateB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
